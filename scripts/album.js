e01c88bcaf73491058147462867ecc737b6a7985
Josiane SU Dufour <jsu.dufour@gmail.com>
2015-09-05 19:38:32 -0400

    Checkpoint 34 completed, added functionality to the seek bars.

--- scripts/album.js
+++ scripts/album.js
@@ -27,6 +27,12 @@ var createSongRow = function (songNumber, songName, songLength) {
             setSong(songNumber);
             updatePlayerBarSong();
             currentSoundFile.play();
+            updateSeekBarWhileSongPlays();
+            
+            var $volumeFill = $('.volume .fill');
+            var $volumeThumb = $('.volume .thumb');
+            $volumeFill.width(currentVolume + '%');
+            $volumeThumb.css({left: currentVolume + '%'});
             
         } else if (currentlyPlayingSongNumber === songNumber) {
       
@@ -96,9 +102,88 @@ var setCurrentAlbum = function(album) {
     
 };  
 
+
+var updateSeekBarWhileSongPlays = function() {
+    
+    if (currentSoundFile) {
+        
+        currentSoundFile.bind('timeupdate', function(event) {
+            
+            var seekBarFillRatio = this.getTime() / this.getDuration();
+            var $seekBar = $('.seek-control .seek-bar');
+            
+            updateSeekPercentage($seekBar, seekBarFillRatio);
+        });
+    }
+};
+
+
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+    
+    var offsetXPercent = seekBarFillRatio * 100;
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+    
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({left: percentageString});
+    
+}
+
+
+var setupSeekBars = function() {
+  
+    var $seekBars = $('.player-bar .seek-bar');
+    
+    $seekBars.click(function(event) {
+        
+        var offsetX = event.pageX - $(this).offset().left;
+        var barWidth = $(this).width();
+        
+        var seekBarFillRatio = offsetX / barWidth;
+        
+        if ($(this).parent().attr('class') == 'seek-control') {
+            seek(seekBarFillRatio * currentSoundFile.getDuration());
+        } else {
+            setVolume(seekBarFillRatio * 100);
+        }
+        
+        updateSeekPercentage($(this), seekBarFillRatio);
+    });
+
+
+    $seekBars.find('.thumb').mousedown(function(event) {
+
+        var $seekBar = $(this).parent();
+
+        $(document).bind('mousemove.thumb', function(event) {
+            var offsetX = event.pageX - $seekBar.offset().left;
+            var barWidth = $seekBar.width();
+            var seekBarFillRatio = offsetX / barWidth;
+            
+            if ($seekBar.parent().attr('class') == 'seek-control') {
+                seek(seekBarFillRatio * currentSoundFile.getDuration());
+            } else {
+                setVolume(seekBarFillRatio);
+            }
+
+            updateSeekPercentage($seekBar, seekBarFillRatio);
+        });
+
+        $(document).bind('mouseup.thumb', function() {
+            $(document).unbind('mousemove.thumb');
+            $(document).unbind('mouseup.thumb');
+        });
+
+    });
+
+};
+
+
 var trackIndex = function(album, song) {
     return album.songs.indexOf(song);
 };
+
 
 var setSong = function(songNumber) {
     
@@ -136,6 +221,14 @@ var setSong = function(songNumber) {
     setVolume(currentVolume);
 
 };
+
+
+var seek = function(time) {
+    
+    if (currentSoundFile) {
+        currentSoundFile.setTime(time);
+    }
+}
 
 
 var setVolume = function(volume) {
@@ -198,6 +291,7 @@ var nextSong = function() {
     //Set a new current song
     setSong(currentSongIndex + 1);
     currentSoundFile.play();
+    updateSeekBarWhileSongPlays();
     //old code before assignment, remove once validated
     //currentlyPlayingSongNumber = currentSongIndex + 1;
     //currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
@@ -232,7 +326,9 @@ var previousSong = function() {
     
     //Set a new current song
     setSong(currentSongIndex + 1);
+    currentSoundFile.play();
     updatePlayerBarSong();
+    updateSeekBarWhileSongPlays();
     //old code before assignment, remove once validated
     //currentlyPlayingSongNumber = currentSongIndex + 1;
     //currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
@@ -256,6 +352,8 @@ var previousSong = function() {
 $(document).ready(function() {
     
     setCurrentAlbum(albumPicasso);
+    setupSeekBars();
+    
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
     
