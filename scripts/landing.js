71ff88c173493a02fa3dbaaf1143a65fd970a1e2
jsudufour <jsudufour@gmail.com>
2015-09-29 00:18:25 -0400

    Progress on assignment 23.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,33 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
+// var animatePoints = function(points) {
         
-    for (var i = 0; i < points.length; i++) {
-        var revealPoints = function() {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
+//     for (var i = 0; i < points.length; i++) {
+//         var revealPoints = function() {
+//             points[i].style.opacity = 1;
+//             points[i].style.transform = "scaleX(1) translateY(0)";
+//             points[i].style.msTransform = "scaleX(1) translateY(0)";
+//             points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+//         };
         
-        revealPoints();
-    }
+//         revealPoints();
+//     }
     
+// };
+
+var animatePoints = function(points) {
+
+    var revealPoints = function() {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+
+    revealPoints();
 };
+
+forEach(pointsArray, animatePoints);
 
 window.onload = function() {
 
