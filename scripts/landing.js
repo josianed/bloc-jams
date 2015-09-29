fc7ca70b6123943e4f45f87b534af1563bfe9038
jsudufour <jsudufour@gmail.com>
2015-09-28 22:39:07 -0400

    Added support for different window sizes for animations on landing page.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,35 @@
-var animatePoints = function() {
-        
-        var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-        for (var i = 0; i < points.length; i++) {
-            var revealPoints = function() {
-                points[i].style.opacity = 1;
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-            };
-            
-            revealPoints();
-        }
+var animatePoints = function(points) {
         
-    };
+    for (var i = 0; i < points.length; i++) {
+        var revealPoints = function() {
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+        
+        revealPoints();
+    }
+    
+};
+
+window.onload = function() {
+
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+
+    if (document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+    }        
+
+    });
+
+}
