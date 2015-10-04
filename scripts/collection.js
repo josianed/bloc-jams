5214cfd5de17a828f7bbb7ae0e0754fcf2e1772a
jsudufour <jsudufour@gmail.com>
2015-10-03 21:57:42 -0400

    Checkpoint 30 completed, refactored album page with jQuery.

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
- var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+   var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,14 +14,20 @@
    + '  </div>'
    + '</div>';
 
-   window.onload = function() {
+   return $(template);
+};
 
-      var collectionContainer = document.getElementsByClassName('album-covers')[0];
 
-      collectionContainer.innerHTML = '';
+   $(window).load(function() {
 
-      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+      var $collectionContainer = $('.album-covers');
+
+      $collectionContainer.empty();
+
+      for (var i = 0; i < 8; i++) {
+         var $newThumbnail = buildCollectionItemTemplate();
+
+         $collectionContainer.append($newThumbnail);
       }
 
-   }
+   });
